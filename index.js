f4ced0e5b1ecd842108d7843b2ae03c926857a7e
William Walsh <wmdwalsh0@gmail.com>
2021-07-07 22:52:48 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    let x = drivers.filter(function(abc){
+        if (abc.toLowerCase() === name.toLowerCase()){
+            return(abc);
+        }
+        
+    })
+    return(x);
+}
+
+function fuzzyMatch(drivers, name){
+    let x = drivers.filter(function(abc){
+        if (abc.substring(0,2) === name.substring(0,2)){
+            return(abc);
+        }
+        
+    })
+    return(x);
+}
+
+function matchName(drivers, name){
+    let x = drivers.filter(function(abc){
+        if (abc.name === name){
+            return(abc);
+        }
+        
+    })
+    return(x);
+}
